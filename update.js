aa40cd41a84c6291331b2402bee39e7a5bb5218b
Tanishq7361 <tanishq7361@gmail.com>
2024-12-27 01:12:50 +0530

    updated

--- update.js
+++ update.js
@@ -1,17 +1,18 @@
-function myfunction(){
-    alert("you clicked");
-}
-  function imge(vl){
-    var pic;
-    if(vl==-1){
-    pic="img/1.jpg"
-    }
-    if(vl==0){
-        pic="img/2.jpg"
-    }
-    if(vl==1){
-        pic="img/3.jpg"
-    }
-    document.getElementById('turn').src = pic;
 
+function imge(vl) {
+  var pic;
+  if (vl == -1) {
+    pic = "img/1.jpg";
+  }
+  if (vl == 0) {
+    pic = "img/2.jpg";
+  }
+  if (vl == 1) {
+    pic = "img/3.jpg";
+  }
+  document.getElementById("turn").src = pic;
+}
+
+  function myFunction(x) {
+    x.classList.toggle("change");
   }
